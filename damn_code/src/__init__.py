6534c29c0282e0b85399a0ca94c3671408450dc0
unknown <priyansh2112@gmail.com>
2025-07-14 14:16:47 +0530

    Added DB

--- damn_code/src/__init__.py
+++ damn_code/src/__init__.py
@@ -1,8 +1,21 @@
 
 from fastapi import FastAPI
 from src.books.routes import book_router
+from contextlib import asynccontextmanager
+from src.db.main import init_db
 
-app = FastAPI()
+@asynccontextmanager
+async def life_span(app:FastAPI):
+    print(f"Server is starting...")
+    await init_db()
+    yield
+    print(f"Server has been stopped")
+
+
+app = FastAPI(
+    title="Priyansh Trying Python",
+    lifespan=life_span
+)
 app.include_router(book_router , prefix="/books")
 
 
